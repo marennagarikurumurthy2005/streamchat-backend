8d4bc595ca1c573526327665c55a88fd6bb269e0
Kurumurthy@2005 <marennagarikurumurthy2005@gmail.com>
2025-08-20 18:28:31 +0530

    added fronted url in settings.py

--- streamchat/settings.py
+++ streamchat/settings.py
@@ -52,6 +52,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # Vite default
     "http://127.0.0.1:5173",
+    "https://streamchatai.netlify.app/",
 ]
 
 # ✅ Allow specific headers
